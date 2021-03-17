3ba28ff0287170cb03a61ccdbc7acf6638e74d51
Andres Auais <andresauais@gmail.com>
2021-03-17 10:52:51 +0000

    ex04

--- src/__tests__/04-exercise.test.js
+++ src/__tests__/04-exercise.test.js
@@ -31,6 +31,10 @@ describe("04-exercises", () => {
      * to reuse the same function return value in differente assertions
      */
 
+    const user = createUserObject("Alex", "Spence", "alex@mail.com");
+    expect(user.firstName).toBeTruthy()
+    expect(user.lastName).toBeTruthy()
+    expect(user.email).toBeTruthy()
     // Write the assertions
   });
 
@@ -45,6 +49,7 @@ describe("04-exercises", () => {
      */
 
     // Write the assertion
+    expect(createCorporateEmailAddress("dani")).toMatch("dani@company.com")
   });
 
   test("generateRandomNumberFrom1to10 returns a number between 1 and 10", () => {
@@ -56,5 +61,7 @@ describe("04-exercises", () => {
      */
 
     // Write the assertions
+    expect(generateRandomNumberFrom1to10()).toBeGreaterThan(0);
+    expect(generateRandomNumberFrom1to10()).toBeLessThan(11);
   });
 });
